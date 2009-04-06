8d7d3e00045ddc895047beae3f3ce05700d68ff4
kunishi <devnull@localhost>
2009-04-06 07:18:44 +0000

    fix adduser.sh not to use cd.

--- scripts/adduser.sh
+++ scripts/adduser.sh
@@ -8,7 +8,6 @@ name=$2
 useradd -c ${name} -d /home/${user} -m -k /etc/skel ${user}
 echo ${user} | passwd --stdin ${user}
 
-cd /var/qmail/users
-echo ${user} >> include
-/var/qmail/bin/qmail-pw2u < /etc/passwd > assign
+echo ${user} >> /var/qmail/users/include
+/var/qmail/bin/qmail-pw2u < /etc/passwd > /var/qmail/users/assign
 /var/qmail/bin/qmail-newu
